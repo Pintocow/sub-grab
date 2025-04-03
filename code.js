b60c988210ee8643ab6d645cd773aaa0b41430ff
Pintocow <pintocow@gmail.com>
2025-04-03 13:52:34 -0600

    added popup.html and some basic code, no functionality yet

--- code.js
+++ code.js
@@ -1,1 +1,14 @@
 
+function mainTask(){
+    browers.tab.reload().then(determineAcceptance);
+}
+
+function determineAcceptance(){
+    //get html of the correct table throw error if not found
+    //apply rejection criteria
+    //apply acceptence criteria
+    //log job when found !!just logging every job for testing!!
+
+}
+
+//fill the options with the currently saved values from a file
