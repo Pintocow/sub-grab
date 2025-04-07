f7c3e736be62d1064c53eff9e2933dd99a7aa9b7
Pintocow <pintocow@gmail.com>
2025-04-07 13:19:01 -0600

    added msg generation reverse engineering

--- content_script.js
+++ content_script.js
@@ -1,18 +1,29 @@
 (() => {
-    alert('this script has run')
     //check if this is already running, if so, leave don't rerun
     if (window.sub_grabHasRun) {
       return;
     }
     window.sub_grabHasRun = true;
-  
+    
+    alert('this script has run')
+
     //grab the jobs from the html and create a message object to send to the listener
     let jobsTable = document.getElementById("availableJobs");
     let jobs = jobsTable.querySelectorAll("tbody.job");
     let message = {type:"list"};
-    for (let i =0; i < jobs.length; i++;){
+    for (let i =0; i < jobs.length; i++){
         let job = jobs[i];
-
+        alert(job);
+        message[i].dateStart = job.querySelector('itemDate').innerHTML;
+        message[i].dateEnd = job.querySelector('multiEndDate').innerHTML;
+        message[i].startTime = job.querySelector('startTime').innerHTML;
+        message[i].endTime = job.querySelector('endTime').innerHTML;
+        message[i].type = job.querySelector('durationName').innerHTML;
+        message[i].locationName = job.querySelector('locationName').innerHTML;
+        message[i].id = job.id;
+        let msg = message[i];
+        alert("thilskdfj" + message[i].type);
+      alert(`${msg.dateStart} - ${msg.dateEnd}  ${msg.startTime} - ${msg.endTime} ${msg.type}  ${msg.locationName}  :: ${msg.id}`);
     }
 
 
